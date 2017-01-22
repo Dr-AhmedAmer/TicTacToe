621d2b5e2b945ce5a7ef3092889bcb5fbaf8f3d6
Ahmed <dr.ahmed.e.amer@gmail.com>
2017-01-22 22:52:36 +0200

    TicTacToe common

--- TicTacToe-Server/src/tictactoe/network/Session.java
+++ TicTacToe-Server/src/tictactoe/network/Session.java
@@ -16,7 +16,6 @@ public class Session implements Runnable{
     
     private Client client;
     private boolean isStarted;
-    private String type;
     private ObjectMapper objectMapper;
     
     public Session(Client client){
@@ -41,55 +40,51 @@ public class Session implements Runnable{
         
         while(isStarted){
             
+            String type = this.client.recieve();
             String msg = this.client.recieve();
-            
-            if(msg != null){
+              
                 
-                if(this.type == null){
-                    
-                    int index = msg.indexOf("=");
-                    
-                    if(index >= 0){
-                        
-                        this.type = msg.substring(index +1);
-                        
-                    }
-                    
-                }else{
-                    
-                    try {
-                        
-                        switch(this.type){
-                            
-                            case MessageTypes.MSG_TYPE_AUTH:
-                              
-                                AuthMessage authMessage = this.objectMapper.readValue(msg, AuthMessage.class);
-                                
-                                ResultObject<Player> authResult = AuthHelper.logIn(authMessage);
-                                
-                                client.send(this.objectMapper.writeValueAsString(authResult));
-                                
-                                break;
-                                
-                            case MessageTypes.MSG_TYPE_REG:
-                                
-                                RegisterMessage registerMessage = this.objectMapper.readValue(msg, RegisterMessage.class);
-                                
-                                ResultObject<Player> regResult = AuthHelper.register(registerMessage);
-                                
-                                client.send(this.objectMapper.writeValueAsString(regResult));
-                                
-                                break;
-                        }
-                        
-                        this.type = null;
-                        
-                    } catch (IOException ex) {
-                            
-                        Logger.getLogger(Session.class.getName()).log(Level.SEVERE, null, ex);
-                    }
-                    
+            if(type != null && msg != null){
+
+            int index = type.indexOf("=");
+
+            if(index >= 0){
+
+                type = type.substring(index +1);
+
+            }
+
+
+            try {
+
+                switch(type){
+
+                    case MessageTypes.MSG_TYPE_AUTH:
+
+                        AuthMessage authMessage = this.objectMapper.readValue(msg, AuthMessage.class);
+
+                        ResultObject<Player> authResult = AuthHelper.logIn(authMessage);
+
+                        client.send(this.objectMapper.writeValueAsString(authResult));
+
+                        break;
+
+                    case MessageTypes.MSG_TYPE_REG:
+
+                        RegisterMessage registerMessage = this.objectMapper.readValue(msg, RegisterMessage.class);
+
+                        ResultObject<Player> regResult = AuthHelper.register(registerMessage);
+
+                        client.send(this.objectMapper.writeValueAsString(regResult));
+
+                        break;
                 }
+
+            } catch (IOException ex) {
+
+                Logger.getLogger(Session.class.getName()).log(Level.SEVERE, null, ex);
+            }
+                   
             }else{
                 
                 this.stop();
