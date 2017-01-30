065fa49e7a993f9f8b5948cdf86a0b9169d195a3
salamaashoush <salamaashoush@gmail.com>
2017-01-30 20:17:48 +0200

    some server and gui intgration

--- TicTacToe-Client/src/tictactoe/gui/Game.java
+++ TicTacToe-Client/src/tictactoe/gui/Game.java
@@ -1,16 +1,28 @@
 package tictactoe.gui;
-	
+
+import com.sun.javafx.scene.control.skin.LabeledText;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
@@ -20,153 +32,272 @@ import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.util.Duration;
+import tictactoe.client.game.GameManager;
+import tictactoe.client.network.SessionManager;
+import tictactoe.models.Player;
 
 public class Game extends Application {
 
-    private boolean playable = true;
-    private boolean turnX = true;
-    private Tile[][] board = new Tile[3][3];
-    private List<Combo> combos = new ArrayList<>();
+	ListView<String> listView = new ListView<String>();
+	private SessionManager sMan = SessionManager.getInstance();
+	private GameManager gMan = GameManager.getInstance();
+	private boolean playable = true;
+	private boolean turnX = true;
+	private Tile[][] board = new Tile[3][3];
+	private List<Combo> combos = new ArrayList<>();
+	private HashMap<String,Integer> playersmap=new HashMap<String,Integer>();
+	private final Image OFFLINE = new Image("file:offline.png");
+	private final Image ONLINE = new Image("file:online.png");
+	private Image[] listOfImages = {ONLINE, OFFLINE};
 
-    private Pane root = new Pane();
+	private SessionManager.GameControlListener gameControlListener = new SessionManager.GameControlListener() {
+		@Override
+		public void onGameRequest(int senderId) {
 
-    private Parent createContent() {
-        root.setPrefSize(600, 600);
+		}
 
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                Tile tile = new Tile();
-                tile.setTranslateX(j * 200);
-                tile.setTranslateY(i * 200);
+		@Override
+		public void onGameResponse(int senderId, int response) {
 
-                root.getChildren().add(tile);
+		}
 
-                board[j][i] = tile;
-            }
-        }
+		@Override
+		public void onPlayerList(List<Player> players) {
+			System.out.println(".onPlayerList()");
+			System.out.println(players.size());
+			if (players == null) {
+				System.out.println("Empty");
+			} else {
+				ObservableList<Player> playerslist = FXCollections.observableArrayList(players);
+				playerslist.forEach(p -> {
+					listView.getItems().add(p.getDisplayName());
+					playersmap.put(p.getDisplayName(), p.getId());
+					
+				});
+				listView.setOnMouseClicked((MouseEvent event) -> {
+						if (event.getButton().equals(MouseButton.PRIMARY)) {
+							if (event.getClickCount() == 2) {
+								
+								sMan.sendInvite(playersmap.get(listView.getSelectionModel()
+                                                    .getSelectedItem()));//your code here  
+								System.out.println("id" + playersmap.get(listView.getSelectionModel()
+                                                    .getSelectedItem()));
+							}
+						}
 
-        // horizontal
-        for (int y = 0; y < 3; y++) {
-            combos.add(new Combo(board[0][y], board[1][y], board[2][y]));
-        }
+					});
 
-        // vertical
-        for (int x = 0; x < 3; x++) {
-            combos.add(new Combo(board[x][0], board[x][1], board[x][2]));
-        }
+				//				listView.setCellFactory(param -> new ListCell<String>() {
+				//					private ImageView imageView = new ImageView();
+				//
+				//					@Override
+				//					public void updateItem(String name, boolean empty) {
+				//						super.updateItem(name, empty);
+				//						if (empty) {
+				//							setText(null);
+				//							setGraphic(null);
+				//						} else {
+				//							if (name.equals("ONLINE")) {
+				//								imageView.setImage(listOfImages[0]);
+				//							} else if (name.equals("OFFLINE")) {
+				//								imageView.setImage(listOfImages[1]);
+				//							}
+				//							setText(name);
+				//							setGraphic(imageView);
+				//						}
+				//					}
+				//				});
+			}
 
-        // diagonals
-        combos.add(new Combo(board[0][0], board[1][1], board[2][2]));
-        combos.add(new Combo(board[2][0], board[1][1], board[0][2]));
+		}
+	};
 
-        return root;
-    }
+	private Pane boardPane = new Pane();
+	BorderPane root = new BorderPane();
+	private GameManager.GameListener gameListener = new GameManager.GameListener() {
+		@Override
+		public void onGameMove(int x, int y) {
+//			if (turnX) {
+//				return;
+//			}
+			board[x][y].drawO();
+			turnX = true;
+			checkState();
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        primaryStage.setScene(new Scene(createContent()));
-        primaryStage.show();
-    }
+		}
 
-    private void checkState() {
-        for (Combo combo : combos) {
-            if (combo.isComplete()) {
-                playable = false;
-                playWinAnimation(combo);
-                break;
-            }
-        }
-    }
+		@Override
+		public void onGameEnd(String winner) {
 
-    private void playWinAnimation(Combo combo) {
-        Line line = new Line();
-        line.setStartX(combo.tiles[0].getCenterX());
-        line.setStartY(combo.tiles[0].getCenterY());
-        line.setEndX(combo.tiles[0].getCenterX());
-        line.setEndY(combo.tiles[0].getCenterY());
+		}
+	};
 
-        root.getChildren().add(line);
+	private Parent createContent() {
+		boardPane.setPrefSize(600, 600);
 
-        Timeline timeline = new Timeline();
-        timeline.getKeyFrames().add(new KeyFrame(Duration.seconds(1),
-                new KeyValue(line.endXProperty(), combo.tiles[2].getCenterX()),
-                new KeyValue(line.endYProperty(), combo.tiles[2].getCenterY())));
-        timeline.play();
-    }
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				Tile tile = new Tile();
+				tile.setTranslateX(j * 200);
+				tile.setTranslateY(i * 200);
+				tile.setX(j);
+				tile.setY(i);
+				boardPane.getChildren().add(tile);
+				board[j][i] = tile;
+			}
+		}
 
-    private class Combo {
-        private Tile[] tiles;
-        public Combo(Tile... tiles) {
-            this.tiles = tiles;
-        }
+		// horizontal
+		for (int y = 0; y < 3; y++) {
+			combos.add(new Combo(board[0][y], board[1][y], board[2][y]));
+		}
 
-        public boolean isComplete() {
-            if (tiles[0].getValue().isEmpty())
-                return false;
+		// vertical
+		for (int x = 0; x < 3; x++) {
+			combos.add(new Combo(board[x][0], board[x][1], board[x][2]));
+		}
 
-            return tiles[0].getValue().equals(tiles[1].getValue())
-                    && tiles[0].getValue().equals(tiles[2].getValue());
-        }
-    }
+		// diagonals
+		combos.add(new Combo(board[0][0], board[1][1], board[2][2]));
+		combos.add(new Combo(board[2][0], board[1][1], board[0][2]));
 
-    private class Tile extends StackPane {
-        private Text text = new Text();
+		boardPane.getChildren().add(listView);
+		root.setCenter(boardPane);
+		root.setRight(listView);
 
-        public Tile() {
-            Rectangle border = new Rectangle(200, 200);
-            border.setFill(null);
-            border.setStroke(Color.BLACK);
+		return root;
+	}
 
-            text.setFont(Font.font(72));
+	@Override
+	public void start(Stage primaryStage) throws Exception {
 
-            setAlignment(Pos.CENTER);
-            getChildren().addAll(border, text);
+		gMan.setGameListener(gameListener);
+		sMan.setGameControlListener(gameControlListener);
+		sMan.sendListPlayers();
+		primaryStage.setScene(new Scene(createContent()));
+		primaryStage.show();
+	}
 
-            setOnMouseClicked(event -> {
-                if (!playable)
-                    return;
+	private void checkState() {
+		for (Combo combo : combos) {
+			if (combo.isComplete()) {
+				playable = false;
+				playWinAnimation(combo);
+				break;
+			}
+		}
+	}
 
-                if (event.getButton() == MouseButton.PRIMARY) {
-                    if (!turnX)
-                        return;
+	private void playWinAnimation(Combo combo) {
+		Line line = new Line();
+		line.setStartX(combo.tiles[0].getCenterX());
+		line.setStartY(combo.tiles[0].getCenterY());
+		line.setEndX(combo.tiles[0].getCenterX());
+		line.setEndY(combo.tiles[0].getCenterY());
 
-                    drawX();
-                    turnX = false;
-                    checkState();
-                }
-                else if (event.getButton() == MouseButton.SECONDARY) {
-                    if (turnX)
-                        return;
+		boardPane.getChildren().add(line);
 
-                    drawO();
-                    turnX = true;
-                    checkState();
-                }
-            });
-        }
+		Timeline timeline = new Timeline();
+		timeline.getKeyFrames().add(new KeyFrame(Duration.seconds(1),
+			new KeyValue(line.endXProperty(), combo.tiles[2].getCenterX()),
+			new KeyValue(line.endYProperty(), combo.tiles[2].getCenterY())));
+		timeline.play();
+	}
 
-        public double getCenterX() {
-            return getTranslateX() + 100;
-        }
+	private class Combo {
 
-        public double getCenterY() {
-            return getTranslateY() + 100;
-        }
+		private Tile[] tiles;
 
-        public String getValue() {
-            return text.getText();
-        }
+		public Combo(Tile... tiles) {
+			this.tiles = tiles;
+		}
 
-        private void drawX() {
-            text.setText("X");
-        }
+		public boolean isComplete() {
+			if (tiles[0].getValue().isEmpty()) {
+				return false;
+			}
 
-        private void drawO() {
-            text.setText("O");
-        }
-    }
+			return tiles[0].getValue().equals(tiles[1].getValue())
+				&& tiles[0].getValue().equals(tiles[2].getValue());
+		}
+	}
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+	private class Tile extends StackPane {
+
+		private Text text = new Text();
+		private int x;
+		private int y;
+
+		public int getX() {
+			return x;
+		}
+
+		public void setX(int x) {
+			this.x = x;
+		}
+
+		public int getY() {
+			return y;
+		}
+
+		public void setY(int y) {
+			this.y = y;
+		}
+
+		public Tile() {
+			Rectangle border = new Rectangle(200, 200);
+			border.setFill(null);
+			border.setStroke(Color.BLACK);
+
+			text.setFont(Font.font(72));
+
+			setAlignment(Pos.CENTER);
+			getChildren().addAll(border, text);
+
+			setOnMouseClicked(event -> {
+				if (!playable) {
+					return;
+				}
+
+				if (event.getButton() == MouseButton.PRIMARY) {
+					if (!turnX) {
+						return;
+					}
+					gMan.move(this.x, this.y);
+					System.out.println(this.x + " " + this.y);
+					drawX();
+					turnX = false;
+					checkState();
+				} else if (event.getButton() == MouseButton.SECONDARY) {
+
+				}
+			});
+		}
+
+		public double getCenterX() {
+			return getTranslateX() + 100;
+		}
+
+		public double getCenterY() {
+			return getTranslateY() + 100;
+		}
+
+		public String getValue() {
+			return text.getText();
+		}
+
+		private void drawX() {
+			text.setText("X");
+		}
+
+		private void drawO() {
+			text.setText("O");
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+
+	}
+
 }
--- TicTacToe-Client/src/tictactoe/gui/Login.java
+++ TicTacToe-Client/src/tictactoe/gui/Login.java
@@ -1,72 +1,112 @@
 package tictactoe.gui;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-import javafx.scene.effect.DropShadow;
-import javafx.scene.effect.Reflection;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
- 
+import tictactoe.client.network.SessionManager;
+import tictactoe.models.Player;
+
 public class Login extends Application {
- 
- String user = "JavaFX2";
- String pw = "password";
- String checkUser, checkPw;
- 
-    public static void main(String[] args) {
-        launch(args);
-    }
-     
-    @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("TicTacToe Login");
-        
-        BorderPane bp = new BorderPane();
-        bp.setPadding(new Insets(10,50,50,50));
-        
-        //Adding HBox
-        HBox hb = new HBox();
-        hb.setPadding(new Insets(20,20,20,30));
-        
-        //Adding GridPane
-        GridPane gridPane = new GridPane();
-        gridPane.setPadding(new Insets(20,20,20,20));
-        gridPane.setHgap(5);
-        gridPane.setVgap(5);
-        
-       //Implementing Nodes for GridPane
-        Label lblUserName = new Label("Username");
-        TextField txtUserName = new TextField();
-        Label lblPassword = new Label("Password");
-        PasswordField pf = new PasswordField();
-        Button btnLogin = new Button("Login");
-	btnLogin.setMaxWidth(Double.MAX_VALUE);
-        Label lblMessage = new Label();
-        
-        //Adding Nodes to GridPane layout
-        gridPane.add(lblUserName, 0, 0);
-        gridPane.add(txtUserName, 1, 0);
-        gridPane.add(lblPassword, 0, 1);
-        gridPane.add(pf, 1, 1);
-        gridPane.add(btnLogin, 0, 2,2,2);
-        gridPane.add(lblMessage, 1, 3);
-        
-                         
-      
-        bp.setTop(hb);
-        bp.setCenter(gridPane);  
-        
-     Scene scene = new Scene(bp);
-     primaryStage.setScene(scene);
-     primaryStage.show();
-    }
+
+	private Stage sta;
+	private SessionManager.AuthListener authListener = new SessionManager.AuthListener() {
+		@Override
+		public void onSuccess(Player p) {
+			try {
+				Platform.runLater(new Runnable() {
+					@Override
+					public void run() {
+						System.out.println("7asal");
+						System.err.println(p.getEmail());
+						Game g = new Game();
+						try {
+							g.start(sta);
+						} catch (Exception ex) {
+							Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+						}
+					}
+				});
+
+			} catch (Exception ex) {
+				Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+			}
+
+		}
+
+		@Override
+		public void onFailure() {
+			System.err.println("ma7asalsh");
+		}
+	};
+	private SessionManager sMan = SessionManager.getInstance();
+
+	String user;
+	String pw;
+	String checkUser, checkPw;
+
+	public static void main(String[] args) {
+		launch(args);
+
+	}
+
+	@Override
+	public void start(Stage primaryStage) {
+		this.sta = primaryStage;
+		primaryStage.setTitle("TicTacToe Login");
+
+		sMan.setAuthListener(authListener);
+
+		BorderPane bp = new BorderPane();
+		bp.setPadding(new Insets(10, 50, 50, 50));
+
+		//Adding HBox
+		HBox hb = new HBox();
+		hb.setPadding(new Insets(20, 20, 20, 30));
+
+		//Adding GridPane
+		GridPane gridPane = new GridPane();
+		gridPane.setPadding(new Insets(20, 20, 20, 20));
+		gridPane.setHgap(5);
+		gridPane.setVgap(5);
+
+		//Implementing Nodes for GridPane
+		Label lblUserName = new Label("Username");
+		TextField txtUserName = new TextField();
+		Label lblPassword = new Label("Password");
+		PasswordField pf = new PasswordField();
+		Button btnLogin = new Button("Login");
+		btnLogin.setMaxWidth(Double.MAX_VALUE);
+		Label lblMessage = new Label();
+
+		//Adding Nodes to GridPane layout
+		gridPane.add(lblUserName, 0, 0);
+		gridPane.add(txtUserName, 1, 0);
+		gridPane.add(lblPassword, 0, 1);
+		gridPane.add(pf, 1, 1);
+		gridPane.add(btnLogin, 0, 2, 2, 2);
+		gridPane.add(lblMessage, 1, 3);
+
+		btnLogin.setOnAction(e -> {
+			user = txtUserName.getText();
+			pw = pf.getText();
+			sMan.login(user, pw);
+		});
+		bp.setTop(hb);
+		bp.setCenter(gridPane);
+
+		Scene scene = new Scene(bp);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
 }
