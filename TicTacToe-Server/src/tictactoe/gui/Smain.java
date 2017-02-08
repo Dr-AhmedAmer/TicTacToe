7eed4dabe9ba20182abfcfc268c96dc4ae6100e0
salamaashoush <salamaashoush@gmail.com>
2017-02-08 16:22:29 +0200

    enhance server gui

--- TicTacToe-Server/src/tictactoe/gui/Smain.java
+++ TicTacToe-Server/src/tictactoe/gui/Smain.java
@@ -4,6 +4,10 @@
  * and open the template in the editor.
  */
 package tictactoe.gui;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXListView;
 import java.util.List;
 import javafx.application.Application;
 import javafx.application.Platform;
@@ -12,7 +16,6 @@ import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
-import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -25,10 +28,14 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.stage.WindowEvent;
 import tictactoe.helpers.PlayerHelper;
-import tictactoe.helpers.ResultList;
 import tictactoe.models.Player;
 import tictactoe.network.Server;
 import tictactoe.network.SessionManager;
@@ -37,131 +44,159 @@ import tictactoe.network.SessionManager;
  *
  * @author lina
  */
-public class Smain extends Application implements SessionManager.PlayerStatusListener  {
-    
-    
-    ListView<Player> listView = new ListView<>();
-    private SessionManager sMan = SessionManager.getInstance();
-    
+public class Smain extends Application implements SessionManager.PlayerStatusListener {
 
-    @Override
-    public void start(Stage primaryStage) {
-        
-        sMan.setPlayerStatusListener(this);
-        Label lport =new Label("Port");
-        TextField port= new TextField();
-        VBox vboxin = new VBox(10);
-        vboxin.minHeight(80);
-         
-        VBox vbox = new VBox(0);
-        
-        BorderPane root =new BorderPane();
-        Button btn = new Button();
-        btn.setText("start server");
-        
-        vbox.getChildren().addAll(vboxin,btn);
-        root.setMargin(vbox, new Insets(80,100,0,100));
-        vbox.setAlignment(Pos.CENTER);
-        root.setCenter(vbox);
-        
-        
-        
-        
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-               Server.start(8000);
-                Platform.runLater(() -> {
-                    
-                genrateListView(PlayerHelper.getAllPlayers().getResults());
-                root.setMargin(listView, new Insets(50,100,50,100));
-                root.setCenter(listView);
-                Button stop = new Button();
-                stop.setText("Stop server");
-                stop.setOnAction(new EventHandler<ActionEvent>() {
-                    @Override
-                    public void handle(ActionEvent event) {
-                        Server.stop();
-                        Platform.exit();
-                        System.exit(0);
-                    }
-                });
-                root.setAlignment(stop, Pos.CENTER);
-                root.setBottom(stop);
-                    
-                    
-                });
-            
-            }
-        });
-        
-        
-        Scene scene = new Scene(root, 400, 500);
-        
-        primaryStage.setTitle("server");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
-    
-    
-    
-    private void genrateListView(List<Player> players) {
-                listView.getItems().clear();
-		ObservableList<Player> playerslist = FXCollections.observableArrayList(players);
-		playerslist.forEach(p -> {
-			listView.getItems().add(p);
+	JFXComboBox filter = new JFXComboBox();
+	HBox btnhb = new HBox();
+	VBox listvb = new VBox();
+	BorderPane root = new BorderPane();
+	JFXButton startbtn = new JFXButton("Start");
+	JFXButton stopbtn = new JFXButton("Stop");
+	JFXButton closebtn = new JFXButton("Close");
+	JFXListView<Player> listView = new JFXListView<>();
+	private SessionManager sMan = SessionManager.getInstance();
+
+	@Override
+	public void start(Stage primaryStage) {
+
+		sMan.setPlayerStatusListener(this);
+		listView.setPrefHeight(550);
+		startbtn.getStyleClass().add("button-raised");
+		stopbtn.getStyleClass().add("button-raised-red");
+		closebtn.getStyleClass().add("button-raised-gray");
+		btnhb.getChildren().addAll(startbtn, stopbtn,closebtn);
+		btnhb.setAlignment(Pos.CENTER);
+		listvb.setPrefHeight(Double.MAX_VALUE);
+		filter.getItems().setAll("Online", "Offline", "All");
+		filter.getSelectionModel().selectFirst();
+		filter.setPrefWidth(Double.MAX_VALUE);
+		listvb.getChildren().addAll(filter, listView);
+		root.setCenter(listvb);
+		root.setBottom(btnhb);
+
+		startbtn.setOnAction((ActionEvent event) -> {
+			Server.start(8000);
+			genrateListView(PlayerHelper.getAllPlayers().getResults());
 		});
-		listView.setCellFactory(parm -> new ListCell<Player>() {
-			private final ImageView imageView = new ImageView();
+		stopbtn.setOnAction((ActionEvent event) -> {
+			Server.stop();
+		});
+		closebtn.setOnAction((event) -> {
+			Server.stop();
+			primaryStage.close();
+		});
+		primaryStage.setOnCloseRequest((WindowEvent event) -> {
+			event.consume();
+		});
+		Scene scene = new Scene(root, 300, 600);
+		scene.getStylesheets().add(getClass().getClassLoader().getResource("assets/style.css").toString());
+		primaryStage.setResizable(false);
+		primaryStage.setTitle("Server");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
 
+	private void genrateListView(List<Player> players) {
+		listView.getStyleClass().add("mylistview");
+		ObservableList<Player> playerslist = FXCollections.observableArrayList(players);
+		filter.setOnAction((event) -> {
+			switch (filter.getSelectionModel().getSelectedItem().toString()) {
+				case "Online":
+					listView.getItems().clear();
+					playerslist.filtered((p) -> p.getStatus().equals("play") || p.getStatus().equals("idle")).forEach((p) -> {
+						listView.getItems().add(p);
+					});
+					break;
+				case "Offline":
+					listView.getItems().clear();
+					playerslist.filtered((p) -> p.getStatus().equals("offln")).forEach((p) -> {
+						listView.getItems().add(p);
+					});
+					break;
+				case "All":
+					listView.getItems().clear();
+					playerslist.forEach(p -> {
+						listView.getItems().add(p);
+					});
+					break;
+				default:
+					break;
+			}
+		});
+		switch (filter.getSelectionModel().getSelectedItem().toString()) {
+			case "Online":
+				listView.getItems().clear();
+				playerslist.filtered((p) -> p.getStatus().equals("play") || p.getStatus().equals("idle")).forEach((p) -> {
+					listView.getItems().add(p);
+				});
+				break;
+			case "Offline":
+				listView.getItems().clear();
+				playerslist.filtered((p) -> p.getStatus().equals("offln")).forEach((p) -> {
+					listView.getItems().add(p);
+				});
+				break;
+			case "All":
+				listView.getItems().clear();
+				playerslist.forEach(p -> {
+					listView.getItems().add(p);
+				});
+				break;
+			default:
+				break;
+		}
+
+		listView.setCellFactory(parm -> new ListCell<Player>() {
 			@Override
 			public void updateItem(Player player, boolean empty) {
-                            
-                            Platform.runLater(() -> {
-                                super.updateItem(player, empty);
+				super.updateItem(player, empty);
 				if (empty) {
 					setText(null);
 					setGraphic(null);
 				} else {
+
 					HBox hBox = new HBox();
 					hBox.setSpacing(3);
+					VBox ptvb = new VBox();
+					ptvb.setAlignment(Pos.CENTER);
+					Text pt = new Text(String.valueOf(player.getPoints()));
+					pt.setFont(Font.font(pt.getFont().toString(), FontWeight.BLACK, FontPosture.ITALIC, 10));
 					Text name = new Text(player.getDisplayName());
-
 					ImageView statusImageView = new ImageView();
 					Image statusImage = new Image(getClass().getClassLoader().getResource("images/" + player.getStatus() + ".png").toString(), 16, 16, true, true);
 					statusImageView.setImage(statusImage);
-
-
-					hBox.getChildren().addAll( statusImageView, name);
+					ptvb.getChildren().addAll(statusImageView, pt, name);
+					ImageView pictureImageView = new ImageView();
+					Image image = new Image(getClass().getClassLoader().getResource("images/avatars/" + player.getImage()).toString(), 50, 50, true, true);
+					pictureImageView.setImage(image);
+					hBox.getChildren().addAll(pictureImageView, ptvb);
 					hBox.setAlignment(Pos.CENTER_LEFT);
 
 					setGraphic(hBox);
 
 				}
-                                
-                            });
-				
+
 			}
 		});
 	}
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        launch(args);
-    }
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		launch(args);
+	}
 
-    @Override
-    public void onPlayerStatusChange(List<Player> list) {
-        System.out.println("onPlayerStatusChange Called");
-        
-        if(list != null){
-            this.genrateListView(list);
-        }else{
-            list = PlayerHelper.getAllPlayers().getResults();
-            this.genrateListView(list);
-        }
-    }
-    
+	@Override
+	public void onPlayerStatusChange(List<Player> list) {
+		if (list != null) {
+			listView.getItems().clear();
+			this.genrateListView(list);
+		} else {
+			listView.getItems().clear();
+			list = PlayerHelper.getAllPlayers().getResults();
+			this.genrateListView(list);
+		}
+	}
+
 }
